9aae346b5e494b4291323432fdc809657c93deee
Moritz Neeb <nt4u@kpvn.de>
2023-08-22 22:27:17 -0400

    ast printer (chapter 5) finished
    Co-authored-by: Nicholas Chen <nichwch@gmail.com>

---
+++ ast_printer.py
@@ -0,0 +1,35 @@
+from scanner import Token
+import expr
+from token_type import *
+
+
+class AstPrinter:
+    def visit_binary_expr(self, expr):
+        left = expr.left.visit(self)
+        right = expr.right.visit(self)
+        return f"({expr.operator.lexeme} {left} {right})"
+
+    def visit_grouping_expr(self, expr):
+        expression = expr.expression.visit(self)
+        return f"(group {expression})"
+
+    def visit_literal_expr(self, expr):
+        if expr.value is None:
+            return "nil"
+        return str(expr.value)
+
+    def visit_unary_expr(self, expr):
+        right = expr.right.visit(self)
+        return f"({expr.operator.lexeme} {right})"
+
+    def print(self, expr):
+        print(expr.visit(self))
+
+
+if __name__ == "__main__":
+    expression = expr.Binary(
+        expr.Unary(Token(MINUS, "-", None, 1), expr.Literal(123)),
+        Token(STAR, "*", None, 1),
+        expr.Grouping(expr.Literal(45.67)),
+    )
+    AstPrinter().print(expression)
---
+++ expr.py
@@ -0,0 +1,60 @@
+from attrs import define, Factory
+from scanner import Token
+
+_grammar = {
+    # fmt: off
+    # "Assign"   : "Token name, Expr value",
+    "Binary"   : "Expr left, Token operator, Expr right",
+    # "Call"     : "Expr callee, Token paren, List<Expr> arguments",
+    # "Get"      : "Expr object, Token name",
+    "Grouping" : "Expr expression",
+    "Literal"  : "Object value",
+    # "Logical"  : "Expr left, Token operator, Expr right",
+    # "Set"      : "Expr object, Token name, Expr value",
+    # "Super"    : "Token keyword, Token method",
+    # "This"     : "Token keyword",
+    "Unary"    : "Token operator, Expr right",
+    # "Variable" : "Token name"
+    # fmt: on
+}
+
+
+@define
+class Expr:
+    pass
+
+
+@define
+class Binary(Expr):
+    left: Expr
+    operator: Token
+    right: Expr
+
+    def visit(self, visitor):
+        return visitor.visit_binary_expr(self)
+
+
+@define
+class Grouping(Expr):
+    expression: Expr
+
+    def visit(self, visitor):
+        return visitor.visit_grouping_expr(self)
+
+
+@define
+class Literal(Expr):
+    # TODO: refine this to what we'd expect (float, bool, str, None)
+    value: object
+
+    def visit(self, visitor):
+        return visitor.visit_literal_expr(self)
+
+
+@define
+class Unary(Expr):
+    operator: Token
+    right: Expr
+
+    def visit(self, visitor):
+        return visitor.visit_unary_expr(self)

711b8f2e6d525db52e9c7ded4432c365334c2fa2
Moritz Neeb <nt4u@kpvn.de>
2023-08-25 18:04:22 -0400

    add runtime error method

--- lox/error.py
+++ lox/error.py
@@ -4,8 +4,15 @@ from .token_type import EOF
 
 
 @define
+class LoxRuntimeError(RuntimeError):
+    token: Token
+    message: str
+
+
+@define
 class ErrorHandler:
     had_error: bool = False
+    had_runtime_error: bool = False
 
     def error(self, line: int, message: str, where: str = ""):
         print(f"[line {line}] Error{where}: {message}")
@@ -16,6 +23,11 @@ class ErrorHandler:
             self.error(token.line, message, where=" at end")
         else:
             self.error(token.line, message, where=f" at '{token.lexeme}'")
+
+    def runtime_error(self, error: LoxRuntimeError) -> None:
+        print(error.message)
+        print(f"[line {error.token.line}]")
+        self.had_runtime_error = True
 
 
 # singleton for globally tracking whether error occurred
